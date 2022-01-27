c59bfe639134746e7959e3c7c016b8bc2ceec5e8
shree675 <shreeteshm37@gmail.com>
2022-01-27 18:01:22 +0530

    added zombies, changed day, added lights, added sounds

---
+++ Game/Assets/Scripts/ZombieCube.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieCube : MonoBehaviour
+{
+
+    public static bool attack=false;
+    private float t;
+
+    void Start(){
+        t=0;
+    }
+
+    void FixedUpdate(){
+        t+=Time.deltaTime;
+        if(t>=5){
+            this.gameObject.transform.rotation=Quaternion.Euler(0,Random.Range(-180.0f,180.0f),0);
+            t=0;
+        }
+        this.gameObject.transform.Translate(Vector3.forward*0.14f*Time.deltaTime);
+    }
+
+    // void OnTriggerStay(Collider collider){
+    //     if(collider.name=="PlayerCube"){
+    //         attack=true;
+    //     }
+    // }
+
+    // void OnTriggerExit(){
+    //     attack=false;
+    // }
+
+    void OnCollisionStay(Collision collision){
+        if(collision.collider.name=="PlayerCube"){
+            HealthBarHandler.SetHealthBarValue(HealthBarHandler.GetHealthBarValue() - 0.001f);
+            attack=true;
+        }
+    }
+
+    void OnCollisionExit(){
+        attack=false;
+    }
+}
