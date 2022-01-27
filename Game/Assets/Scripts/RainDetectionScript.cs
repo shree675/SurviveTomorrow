2a85d21e7fe1f4515b462f64ba42b174541c7d3c
AASHRITH1903 <cs19b025@iittp.ac.in>
2022-01-27 23:01:49 +0530

    Health deterioration functionality for Acid rain

--- Game/Assets/Scripts/RainDetectionScript.cs
+++ Game/Assets/Scripts/RainDetectionScript.cs
@@ -7,7 +7,6 @@ public class RainDetectionScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -18,22 +17,7 @@ public class RainDetectionScript : MonoBehaviour
 
     void OnParticleCollision(GameObject other){
 
-        // int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
-
-        // Rigidbody rb = other.GetComponent<Rigidbody>();
-        // int i = 0;
-
-        // while (i < numCollisionEvents)
-        // {
-        //     if (rb)
-        //     {
-        //         Vector3 pos = collisionEvents[i].intersection;
-        //         Vector3 force = collisionEvents[i].velocity * 10;
-        //         rb.AddForce(force);
-        //     }
-        //     i++;
-        // }
-
-        Debug.Log("!!!!!");
+        HealthBarHandler.SetHealthBarValue(HealthBarHandler.GetHealthBarValue() - 0.02f);
+    
     }
 }
